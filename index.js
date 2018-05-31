d8bc226708ab0806caf3bf1bc6d199456a109b66
Sean Clifford <sclifford6262@gmail.com>
2018-05-31 21:37:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function createPost(){
+  const author = document.getElementById("postAuthor").value
+  const title = document.getElementById("postTitle").value
+  const body = document.getElementById("postBody").value
+
+  let postTemplate = document.getElementById("post-template").innerHTML
+  const postTemplateFn = _.template(postTemplate)
+
+  let pageTemplate = document.getElementById("page-template").innerHTML
+  const pageTemplateFn = _.template(pageTemplate)
+
+  const postTemplateHTML = postTemplateFn({'author': author, 'title': title, 'body': body})
+  const pageTemplateHTML = pageTemplateFn({'post': postTemplateHTML})
+
+  const mainHTML = document.getElementsByTagName('main')[0]
+  mainHTML.innerHTML += pageTemplateHTML
+}
+
+function postComment(){
+  
+}
